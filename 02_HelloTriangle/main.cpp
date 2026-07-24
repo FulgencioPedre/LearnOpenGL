b3bc598f9bfe7523eb4aff0a219036b549b67af3
Fulgenciopr <fulgenciopr@hotmail.com>
2026-07-24 13:51:35 +0200

    Implemented VAO, VBO and EBO, and all linkings vertex attributes to generate any 2d figure

--- 02_HelloTriangle/main.cpp
+++ 02_HelloTriangle/main.cpp
@@ -3,6 +3,21 @@
 #include<glfw/glfw3.h>
 using namespace std;
 
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.3f, 0.5f, 1.0f);\n"
+"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
@@ -13,7 +28,41 @@ void closeKey(GLFWwindow* window) {
 	}
 }
 
+void checkCompilationShaders(unsigned int shader, bool isProgram) {
+	int success;
+	char infoLog[512];
+	if (isProgram) {
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shader, 512, NULL, infoLog);
+			cout << "Compilation Failed" << infoLog << endl;
+		}
+	}
+	else {
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(shader, 512, NULL, infoLog);
+			cout << "Compilation_Failed" << infoLog << endl;
+		}
+	}
+}
+
 int main() {
+
+	float vertices[] = {
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+	
+	
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -23,8 +72,14 @@ int main() {
 	if (window == nullptr) {
 		cout << "Window was not able to instatiate" << endl;
 		glfwTerminate();
+		const char* description;
+		int error = glfwGetError(&description);
+
+		cout << "Error: " << error << endl;
+		cout << description << endl;
 		return -1;
 	}
+	
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -34,11 +89,78 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//VAO
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	//VBO
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	//EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	//CONFIGURE VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	//CONFIGURE EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//CONFIGURE VAO ATTRIBUTES
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//VERTEX SHADER
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Used to check if compilation of the shader was successful
+	checkCompilationShaders(vertexShader, false);
+	
+
+	//FRAGMENT SHADER
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	checkCompilationShaders(fragmentShader, false);
+
+
+	//SHADER PROGRAM OBJECT
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	checkCompilationShaders(shaderProgram, true);
+	
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
 	while (!glfwWindowShouldClose(window)) {
 		closeKey(window);
 
 		glClearColor(0.3f, 0.5f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
